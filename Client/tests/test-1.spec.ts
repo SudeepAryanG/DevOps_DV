6c712fa41fe65cd88e26f67fa21dfccbababf524
Sudeeparyan <sudeeparyang@gmail.com>
2023-08-11 14:24:26 +0530

    test

--- Client/tests/test-1.spec.ts
+++ Client/tests/test-1.spec.ts
@@ -4,7 +4,5 @@ test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await page.locator('div').filter({ hasText: 'Device Vision' }).nth(2).click();
   await page.getByText('Upload / Drag and drop the Actual Dataset for Testing').click();
-  await page.getByText('Upload / Drag and drop the Actual Dataset for Testing').click();
-  await page.locator('div').filter({ hasText: /^New Project$/ }).first().click();
-  await page.locator('button').click();
+  await page.getByText('Upload / Drag and drop the Actual Dataset for TestingNew Project').click();
 });
