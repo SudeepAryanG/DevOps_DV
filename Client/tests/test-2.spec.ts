3912e08622f6495ae7696b000127a2e00864a4d6
Sudeeparyan <sudeeparyang@gmail.com>
2023-08-17 14:30:36 +0530

    test

---
+++ Client/tests/test-2.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.locator('div').filter({ hasText: 'Device Vision' }).nth(2).click();
+  await page.getByText('Upload / Drag and drop the Actual Dataset for Testing').click();
+  await page.getByText('Upload / Drag and drop the Actual Dataset for TestingNew Project').click();
+});
